e579651575eb32de490faa204dca6dae7d637409
Sacred Heart <john@Sacreds-MacBook-Pro.local>
2022-06-30 22:44:51 +0530

    Setup proxy js

---
+++ src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware(
+      '/backend/search/products',
+      {
+        target: 'https://www.blibli.com',
+        changeOrigin: true,
+      }
+    )
+  );
+};
