cbabdc6fe53654495ea2968d11afa7dd6425d9e7
Sacred Heart <john@Sacreds-MacBook-Pro.local>
2022-07-01 13:37:18 +0530

    remove proxy

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
 export const call = (json) => {
-  return axios.get(`https://www.blibli.com/backend/search/products?searchTerm=${json.searchTerm}&start=${json.page}&itemPerPage=${json.itemPerPage}`);
+  return axios.get(`https://www.blibli.com/backend/search/products?searchTerm=${json.searchTerm}&start=${json.page}&itemPerPage=${json.itemPerPage}`, {
+    headers: {
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*"
+    }
+  });
 }
