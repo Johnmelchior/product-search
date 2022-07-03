27b0b9472630d8e04dc3480713d4d89ae23c7f11
Sacred Heart <john@Sacreds-MacBook-Pro.local>
2022-07-03 12:24:58 +0530

    Test heroku direct

--- api/server.js
+++ api/server.js
@@ -1,66 +1,66 @@
-const proxy = require("express-http-proxy");
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const proxy = require("express-http-proxy");
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const express = require('express'),
-  path = require('path'),
-  app = express(),
-  bodyParser = require("body-parser"),
-  port = process.env.PORT || 3070;
+// const express = require('express'),
+//   path = require('path'),
+//   app = express(),
+//   bodyParser = require("body-parser"),
+//   port = process.env.PORT || 3070;
 
-const origin = process.env.ORIGIN || "*";
+// const origin = process.env.ORIGIN || "*";
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../build')));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, '../build')));
 
-/*app.use(
-  "/service/blibliserver",
-  proxy("https://www.blibli.com", {
-    proxyReqOptDecorator(proxyReqOpts) {
-      proxyReqOpts.headers["Origin"] = "https://www.blibli.com";
-      return proxyReqOpts;
-    },
-    proxyErrorHandler: function(err, res, next) {
-      console.log("Proxy err", err);
-      next(err);
-    }
-  })
-)*/
+// /*app.use(
+//   "/service/blibliserver",
+//   proxy("https://www.blibli.com", {
+//     proxyReqOptDecorator(proxyReqOpts) {
+//       proxyReqOpts.headers["Origin"] = "https://www.blibli.com";
+//       return proxyReqOpts;
+//     },
+//     proxyErrorHandler: function(err, res, next) {
+//       console.log("Proxy err", err);
+//       next(err);
+//     }
+//   })
+// )*/
 
-/*app.get('/backend/search/products', (req, res) => {
-  const json = req.query;
-  request(`https://www.blibli.com/backend/search/products?searchTerm=${json.searchTerm}&start=${json.start}&itemPerPage=${json.itemPerPage}`,
-    (err, res1, data) => {
-      console.log(err, res1, data);
-      res.json(res1);
-    }, (error) => console.error(error));
-});*/
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", origin);
-  next();
-});
+// /*app.get('/backend/search/products', (req, res) => {
+//   const json = req.query;
+//   request(`https://www.blibli.com/backend/search/products?searchTerm=${json.searchTerm}&start=${json.start}&itemPerPage=${json.itemPerPage}`,
+//     (err, res1, data) => {
+//       console.log(err, res1, data);
+//       res.json(res1);
+//     }, (error) => console.error(error));
+// });*/
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", origin);
+//   next();
+// });
 
-app.use(
-  createProxyMiddleware(
-    '/service/blibliserver',
-    {
-      target: 'https://www.blibli.com',
-      headers: {authority: 'www.blibli.com', referer: ''},
-      // on : {
-      //   proxyReq: (proxyReq, req, res) => {
-      //     proxyReq.setHeader('Origin', 'https://www.blibli.com');
-      //   },
-      // },
-      pathRewrite: {
-        '^/service/blibliserver': '', // rewrite path
-      },
-      changeOrigin: true,
-      logger: console,
-      secure: false
-    }
-  )
-);
+// app.use(
+//   createProxyMiddleware(
+//     '/service/blibliserver',
+//     {
+//       target: 'https://www.blibli.com',
+//       headers: {authority: 'www.blibli.com', referer: ''},
+//       // on : {
+//       //   proxyReq: (proxyReq, req, res) => {
+//       //     proxyReq.setHeader('Origin', 'https://www.blibli.com');
+//       //   },
+//       // },
+//       pathRewrite: {
+//         '^/service/blibliserver': '', // rewrite path
+//       },
+//       changeOrigin: true,
+//       logger: console,
+//       secure: false
+//     }
+//   )
+// );
 
-app.listen(port, () => {
-  console.log(`Server listening on the port::${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server listening on the port::${port}`);
+// });
